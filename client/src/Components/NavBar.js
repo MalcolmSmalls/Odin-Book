775e23348e6c41a36310631b09ee2702f91fdf94
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-10 22:09:16 -0800

    Finish jsx for navBar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,12 +1,26 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas, faR } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faR, faUserGroup, faUsers, faBars, faBell } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons'
 
 export default function NavBar(props){
     return (
         <nav>
             <FontAwesomeIcon icon={faR} />
-            Hello
+            <input className = "searchbar" 
+                   type = "text"
+                   placeholder = "Search Rapbook"
+            />
+            <FontAwesomeIcon icon={faHouse} />
+            <FontAwesomeIcon icon={faUserGroup} />
+            <FontAwesomeIcon icon={faUsers} />
+
+            <button>Find Friends</button>
+
+            <button><FontAwesomeIcon icon={faBars} /></button>
+            <button><FontAwesomeIcon icon={faFacebookMessenger} /></button>
+            <button><FontAwesomeIcon icon={faBell} /></button>
+
         </nav>
     )
 }
